1dcdf5ad595f1c563927467583b467dd8fda136a
imadunic <imadun00@fesb.hr>
2022-06-05 14:51:22 +0200

    Adding favorites enabled

--- components/characterCard/CharacterCard.js
+++ components/characterCard/CharacterCard.js
@@ -1,11 +1,27 @@
 import Image from "next/image";
 import styles from "./CharacterCard.module.css";
+import { FaRegBookmark } from "react-icons/fa";
 
-const CharacterCard = ({ character }) => {
+const CharacterCard = ({ character, favoriteFlag }) => {
   let imageURL =
     character.thumbnail.path +
     "/portrait_incredible." +
     character.thumbnail.extension;
+
+  const handleBookmarkClick = () => {
+    favoriteFlag = !favoriteFlag;
+    let favorites = JSON.parse(localStorage.getItem("favorites"));
+    if (!favorites) favorites = [];
+
+    // Add/remove from array of favorites
+    if (favoriteFlag) favorites = [...favorites, character.id];
+    else {
+      let index = favorites.indexOf(character.id);
+      if (index > -1) favorites.splice(index, 1); // 2nd parameter means remove one item only
+    }
+
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  };
 
   return (
     <div className={styles.cardWrapper}>
@@ -20,6 +36,10 @@ const CharacterCard = ({ character }) => {
 
       <div>
         <p className={styles.characterName}>{character.name}</p>
+        <FaRegBookmark
+          onClick={handleBookmarkClick}
+          className={"bookmarkIcon"}
+        />
       </div>
     </div>
   );
--- components/characterContainer/CharacterContainer.js
+++ components/characterContainer/CharacterContainer.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import CharacterCard from "../characterCard/CharacterCard";
 import styles from "./CharacterContainer.module.css";
 
 const CharacterContainer = ({ characters }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    let fetchedFavorites = localStorage.getItem("favorites");
+    if (fetchedFavorites) setFavorites(JSON.parse(fetchedFavorites));
+  }, [characters]);
+
   return (
     <div className={styles.characterContainer}>
       {characters.map((character) => (
-        <CharacterCard key={character.id} character={character} />
+        <CharacterCard
+          key={character.id}
+          character={character}
+          favoriteFlag={favorites.indexOf(character.id) >= 0 ? true : false}
+        />
       ))}
     </div>
   );
